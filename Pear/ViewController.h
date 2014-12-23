f2e64d9affa60fe11001f57f273e94c4fa7a0c73
Kevin Hwang <k3vinhwang@gmail.com>
2014-12-23 13:02:47 -0800

    Initial Commit

---
+++ Pear/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pear
+//
+//  Created by Kevin Hwang on 12/23/14.
+//  Copyright (c) 2014 Kevin Hwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Pear/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Pear
+//
+//  Created by Kevin Hwang on 12/23/14.
+//  Copyright (c) 2014 Kevin Hwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
