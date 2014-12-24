316a89ed17e1774c88807d839eced69e5e6a454f
Kevin Hwang <k3vinhwang@gmail.com>
2014-12-24 12:46:09 -0800

    Namespace the app delegate

---
+++ Pear/KHAppDelegate.m
@@ -0,0 +1,13 @@
+//
+//  KHAppDelegate.m
+//  Pear
+//
+//  Created by Kevin Hwang on 12/24/14.
+//  Copyright (c) 2014 Kevin Hwang. All rights reserved.
+//
+
+#import "KHAppDelegate.h"
+
+@implementation KHAppDelegate
+
+@end
