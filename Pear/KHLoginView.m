8aab926b59fae89689b1b495a1661935640fc5e0
Kevin Hwang <k3vinhwang@gmail.com>
2014-12-25 10:23:13 -0800

    Add login view

--- Pear/KHLoginView.m
+++ Pear/KHLoginView.m
@@ -7,14 +7,69 @@
 //
 
 #import "KHLoginView.h"
+#import "Masonry.h"
+
+@interface KHLoginView()
+
+@property (nonatomic, strong) UITextField *usernameField;
+@property (nonatomic, strong) UITextField *passwordField;
+@property (nonatomic, strong) UIButton *signInButton;
+@property (nonatomic, strong) UIButton *signUpButton;
+
+@end
 
 @implementation KHLoginView
 
 - (instancetype)initWithFrame:(CGRect)frame {
     if (self = [super initWithFrame:frame]) {
-        self.backgroundColor = [UIColor greenColor];
+        _usernameField = [[UITextField alloc] init];
+        _usernameField.placeholder = NSLocalizedString(@"Username", nil);
+        [self addSubview:_usernameField];
+        
+        _passwordField = [[UITextField alloc] init];
+        _passwordField.placeholder = NSLocalizedString(@"Password", nil);
+        [self addSubview:_passwordField];
+        
+        _signInButton = [[UIButton alloc] init];
+        [_signInButton setTitle:NSLocalizedString(@"Sign in", nil) forState:UIControlStateNormal];
+        [self addSubview:_signInButton];
+        
+        _signUpButton = [[UIButton alloc] init];
+        [_signUpButton setTitle:NSLocalizedString(@"Sign up", nil) forState:UIControlStateNormal];
+        [self addSubview:_signUpButton];
+        
+        [self _initializeAutolayout];
     }
     return self;
+}
+
+- (void)_initializeAutolayout {
+    CGFloat padding = 40.0f;
+    
+    [self.usernameField mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self);
+        make.bottom.equalTo(self.passwordField.mas_top);
+        make.left.equalTo(self).with.offset(padding);
+        make.right.equalTo(self).with.offset(-padding);
+    }];
+    
+    [self.passwordField mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.center.equalTo(self);
+        make.width.equalTo(self.usernameField);
+    }];
+    
+    [self.signInButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self);
+        make.top.equalTo(self.passwordField.mas_bottom);
+        make.width.equalTo(self.usernameField);
+    }];
+    
+    [self.signUpButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self);
+        make.top.equalTo(self.signInButton.mas_bottom);
+        make.width.equalTo(self.usernameField);
+    }];
+    
 }
 
 @end
