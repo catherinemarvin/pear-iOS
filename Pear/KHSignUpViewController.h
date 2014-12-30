fee53d49bb44d670b733f04d7f58d4f2159c8ab0
Kevin Hwang <k3vinhwang@gmail.com>
2014-12-29 23:38:12 -0800

    Create a SignUp VC

---
+++ Pear/KHSignUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  KHSignUpViewController.h
+//  Pear
+//
+//  Created by Kevin Hwang on 12/29/14.
+//  Copyright (c) 2014 Kevin Hwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KHSignUpViewController : UIViewController
+
+@end
