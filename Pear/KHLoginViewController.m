645f1c202d0f08d5afc0b27230fcdd31c2af2d54
Kevin Hwang <k3vinhwang@gmail.com>
2014-12-29 19:46:11 -0800

    Move the keyboard up

--- Pear/KHLoginViewController.m
+++ Pear/KHLoginViewController.m
@@ -12,6 +12,8 @@
 @interface KHLoginViewController ()<UITextFieldDelegate>
 
 @property (nonatomic, strong) UIScrollView *scrollView;
+@property (nonatomic, strong) KHLoginView *loginView;
+
 @property (nonatomic, strong) UITextField *activeField;
 
 @end
@@ -32,19 +34,20 @@
     scroll.contentSize = loginView.bounds.size;
     
     self.scrollView = scroll;
+    self.loginView = loginView;
     self.view = scroll;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_keyboardDidShow:) name:UIKeyboardDidShowNotification object:self.view.window];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_keyboardWillShow:) name:UIKeyboardWillShowNotification object:self.view.window];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_keyboardWillHide:) name:UIKeyboardWillHideNotification object:self.view.window];
 }
 
 #pragma mark - Keyboard Notifications
 
-- (void)_keyboardDidShow:(NSNotification *)n {
+- (void)_keyboardWillShow:(NSNotification *)n {
     NSDictionary *userInfo = [n userInfo];
     
     CGSize keyboardSize = [[userInfo objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
@@ -53,6 +56,16 @@
     self.scrollView.contentInset = contentInsets;
     self.scrollView.scrollIndicatorInsets = contentInsets;
     
+    CGRect aRect = self.view.frame;
+    aRect.size.height -= keyboardSize.height;
+    
+    CGPoint origin = self.loginView.signUpButton.frame.origin;
+    origin.y += CGRectGetHeight(self.loginView.signUpButton.frame);
+    
+    if (!CGRectContainsPoint(aRect, origin)) {
+        CGPoint scrollPoint = CGPointMake(0, self.loginView.signUpButton.frame.origin.y + self.loginView.signUpButton.frame.size.height - (aRect.size.height));
+        [self.scrollView setContentOffset:scrollPoint animated:YES];
+    }
 }
 
 - (void)_keyboardWillHide:(NSNotification *)n {
