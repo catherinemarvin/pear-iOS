f46c0a1ffe8fa6b6195e48c6f16bc4c0418eeb64
Kevin Hwang <k3vinhwang@gmail.com>
2015-01-03 13:49:31 -0800

    Add a house chores VC to view chores for an entire house

---
+++ Pear/KHHouseChoresViewController.h
@@ -0,0 +1,13 @@
+//
+//  KHHouseChoresViewController.h
+//  Pear
+//
+//  Created by Kevin Hwang on 1/3/15.
+//  Copyright (c) 2015 Kevin Hwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KHHouseChoresViewController : UIViewController
+
+@end
---
+++ Pear/KHHouseChoresViewController.m
@@ -0,0 +1,58 @@
+//
+//  KHHouseChoresViewController.m
+//  Pear
+//
+//  Created by Kevin Hwang on 1/3/15.
+//  Copyright (c) 2015 Kevin Hwang. All rights reserved.
+//
+
+#import "KHHouseChoresViewController.h"
+
+@interface KHHouseChoresViewController ()<UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic, strong) UITableView *tableView;
+
+@end
+
+static NSString *KHkHouseChoresCellIdentifier = @"houseChoresCell";
+
+@implementation KHHouseChoresViewController
+
+- (void)loadView {
+    CGRect frame = [UIScreen mainScreen].applicationFrame;
+    
+    UITableView *tableView = [[UITableView alloc] initWithFrame:frame style:UITableViewStylePlain];
+    
+    tableView.delegate = self;
+    tableView.dataSource = self;
+    
+    self.tableView = tableView;
+    self.view = tableView;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    if (section == 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:KHkHouseChoresCellIdentifier forIndexPath:indexPath];
+    
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+@end
