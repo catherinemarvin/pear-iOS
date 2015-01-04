ab445f53a4526146f453d81d71c68300d50c11b8
Kevin Hwang <k3vinhwang@gmail.com>
2015-01-04 15:01:15 -0800

    Create chore VC to view chore information

---
+++ Pear/KHChoreViewController.h
@@ -0,0 +1,13 @@
+//
+//  KHChoreViewController.h
+//  Pear
+//
+//  Created by Kevin Hwang on 1/4/15.
+//  Copyright (c) 2015 Kevin Hwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KHChoreViewController : UIViewController
+
+@end
---
+++ Pear/KHChoreViewController.m
@@ -0,0 +1,37 @@
+//
+//  KHChoreViewController.m
+//  Pear
+//
+//  Created by Kevin Hwang on 1/4/15.
+//  Copyright (c) 2015 Kevin Hwang. All rights reserved.
+//
+
+#import "KHChoreViewController.h"
+
+@interface KHChoreViewController ()
+
+@end
+
+@implementation KHChoreViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
