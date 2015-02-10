833238134489771915579b8dbfb08b8dba645130
Kevin Hwang <k3vinhwang@gmail.com>
2015-02-09 19:29:44 -0800

    Add a UIPageViewController for the feature intro

---
+++ Pear/KHFeatureIntroViewController.h
@@ -0,0 +1,13 @@
+//
+//  KHFeatureIntroViewController.h
+//  Pear
+//
+//  Created by Kevin Hwang on 2/9/15.
+//  Copyright (c) 2015 Kevin Hwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KHFeatureIntroViewController : UIPageViewController
+
+@end
