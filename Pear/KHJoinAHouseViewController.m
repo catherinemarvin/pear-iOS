9cf04a2cbf96dab0dd85398f8d2d59ee24dc475b
Kevin Hwang <k3vinhwang@gmail.com>
2015-03-04 22:08:58 -0800

    Add viewmodel to the vc

--- Pear/KHJoinAHouseViewController.m
+++ Pear/KHJoinAHouseViewController.m
@@ -8,30 +8,30 @@
 
 #import "KHJoinAHouseViewController.h"
 
+// ViewModel
+#import "KHJoinAHouseViewModel.h"
+
 @interface KHJoinAHouseViewController ()
+
+@property (nonatomic, strong) KHJoinAHouseViewModel *viewModel;
 
 @end
 
 @implementation KHJoinAHouseViewController
 
+- (instancetype)init {
+    if (self = [super init]) {
+        _viewModel = [[KHJoinAHouseViewModel alloc] init];
+    }
+    return self;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self _bindViewModel];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)_bindViewModel {
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
