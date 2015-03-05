ccc26d29c43b92d2d950085f38eb94a1445ff22d
Kevin Hwang <k3vinhwang@gmail.com>
2015-03-04 21:47:47 -0800

    Add a KHTabBarController to hold the main content of the app

---
+++ Pear/KHTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  KHTabBarController.h
+//  Pear
+//
+//  Created by Kevin Hwang on 3/4/15.
+//  Copyright (c) 2015 Kevin Hwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KHTabBarController : UITabBarController
+
+@end
---
+++ Pear/KHTabBarController.m
@@ -0,0 +1,37 @@
+//
+//  KHTabBarController.m
+//  Pear
+//
+//  Created by Kevin Hwang on 3/4/15.
+//  Copyright (c) 2015 Kevin Hwang. All rights reserved.
+//
+
+#import "KHTabBarController.h"
+
+@interface KHTabBarController ()
+
+@end
+
+@implementation KHTabBarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
